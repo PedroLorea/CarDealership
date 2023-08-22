619640d2fc5437a446c5f3f03e975dd9a01f3629
PedroLorea <p.lorea@edu.pucrs.br>
2023-08-22 09:46:48 -0300

    Linhas comentadas remove

--- src/main/java/br/portoalegre/CardealershipApplication.java
+++ src/main/java/br/portoalegre/CardealershipApplication.java
@@ -2,10 +2,8 @@ package br.portoalegre;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-// @EntityScan(basePackages = "br.portoalegre.car")
 public class CardealershipApplication {
 
 	public static void main(String[] args) {
